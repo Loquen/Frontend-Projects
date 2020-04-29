beb7f3a93293c2d06b76ce545c06dc450b0c6eae
Loquen Jones <lrjones416@gmail.com>
2020-04-29 09:25:12 -0700

    add js controls to faq and features

---
+++ bookmark-landing-page-master/main.js
@@ -0,0 +1,43 @@
+const questionsEl = document.querySelector('.faq-list');
+const selectorEl = document.querySelector('.selector');
+
+// Helper function 
+function chooseNextSelection(className){
+  let selection = document.querySelectorAll(`.${className}`);
+  selection.forEach(el => {
+    el.classList.add('selected');
+  });
+}
+
+questionsEl.addEventListener('click', e => {
+  if(!e.target.className === 'question'){
+    return;
+  }
+
+  if(e.target.id){
+    const selectedQuestionId = e.target.id.replace('q', 'a');
+    const selectedQuestionEl = document.getElementById(selectedQuestionId);
+    
+    selectedQuestionEl.classList.toggle('hidden');
+  }
+});
+
+selectorEl.addEventListener('click', e => {
+  // Find any element with the selected class and remove it
+  let selected = document.querySelectorAll('.selected');
+
+  selected.forEach(el => {
+    el.classList.remove('selected');
+  });
+
+  const selectionClassList = e.target.classList;
+
+  if([...selectionClassList].includes('bookmark')){
+    chooseNextSelection('bookmark');
+  } else if ([...selectionClassList].includes('search')){
+    chooseNextSelection('search');
+  } else {
+    chooseNextSelection('share');
+  }
+
+});
